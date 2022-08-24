148bc74e01971efaccb640819cb92e8f43bca001
Peram Najafi <pedram.najafi73@yahoo.com>
2022-08-24 20:05:16 +0430

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   core/blog/api/v1/views.py

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -5,23 +5,73 @@ from .serializers import PostSerializers
 from blog.models import Post
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
+from rest_framework.views import APIView 
+from rest_framework.generics import GenericAPIView , ListCreateAPIView
+from rest_framework import mixins
+from rest_framework.generics import ListCreateAPIView
+
+
+# """ list and create post from APIView  """
+# class PostList(APIView):
+#     # permission_classes = [IsAuthenticated]
+#     serializer_class = PostSerializers
+#     def get(self , request):
+#         posts = Post.objects.filter(status = True)
+#         serializer = PostSerializers(posts , many =True)
+#         return Response(serializer.data)
+
+#     def post(self , request):
+#         serializer =PostSerializers(data = request.data)
+#         serializer.is_valid(raise_exception= True)
+#         serializer.save()
+#         return Response(serializer.data)
+
+
+# """ list and create post from GenericAPIView """
+
+# class PostList(GenericAPIView):
+#     permission_classes = []
+#     serializer_class = PostSerializers
+#     queryset = Post.objects.filter(status = True)
+
+#     def get (self , request):
+#         queryset = self.get_queryset()
+#         serializer = self.serializer_class(queryset , many=True)
+#         return Response(serializer.data)
+
+#     def post(self , request):
+#         serializer =self.serializer_class(data = request.data)
+#         serializer.is_valid(raise_exception= True)
+#         serializer.save()
+#         return Response(serializer.data)
 
 
 
-class PostList(APIView):
-    permission_classes = [IsAuthenticated]
+
+# """ list and create post from ListModelMixin and CreateModelMixin """
+
+# class PostList(GenericAPIView , mixins.ListModelMixin , mixins.CreateModelMixin):
+#     permission_classes = []
+#     serializer_class = PostSerializers
+#     queryset = Post.objects.filter (status = True)
+
+#     def get(self , request , *args , **kwargs):
+#         return self.list(request , *args , **kwargs)
+
+#     def post(self ,request , *args , **kwargs):
+#         return self.create(request , *args , **kwargs)
+
+
+""" list and create post from ListCreateAPIView """
+
+class PostList(ListCreateAPIView):
+    permission_classes =[]
     serializer_class = PostSerializers
-    def get(self , request):
-        posts = Post.objects.filter(status = True)
-        serializer = PostSerializers(posts , many =True)
-        return Response(serializer.data)
+    queryset = Post.objects.filter(status = True)
 
-    def post(self , request):
-        serializer =PostSerializers(data = request.data)
-        serializer.is_valid(raise_exception= True)
-        serializer.save()
-        return Response(serializer.data)
+
+
+
 
 
 class PostDetail(APIView):
