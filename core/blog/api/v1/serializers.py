06b12b69ec1339a0d796b426e667a97fbe63510c
Peram Najafi <pedram.najafi73@yahoo.com>
2022-08-29 13:30:42 +0430

    ModelViewSet

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Category, Post
 
 
 
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -201,29 +201,29 @@ from rest_framework import viewsets
 
 """ ViewSets """
 
-class PostViewSet(viewsets.ViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = []
     serializer_class = PostSerializers
     queryset = Post.objects.filter (status = True)
 
-    def list(self , request):
-        serializer = self.serializer_class(self.queryset , many= True)
-        return Response (serializer.data)
+    # def list(self , request):
+    #     serializer = self.serializer_class(self.queryset , many= True)
+    #     return Response (serializer.data)
 
-    def retrieve(self , request , pk=None):
-        post_object = get_object_or_404(self.queryset , pk=pk)
-        serializer = self.serializer_class(post_object)
-        return Response(serializer.data)
+    # def retrieve(self , request , pk=None):
+    #     post_object = get_object_or_404(self.queryset , pk=pk)
+    #     serializer = self.serializer_class(post_object)
+    #     return Response(serializer.data)
 
 
-    def create(self , request , pk = None):
-        pass
+    # def create(self , request , pk = None):
+    #     pass
 
-    def update(self , request , pk=None):
-        pass 
+    # def update(self , request , pk=None):
+    #     pass 
 
-    def partial_update (self , request ,pk = None):
-        pass
+    # def partial_update (self , request ,pk = None):
+    #     pass
 
-    def destroy (self , request , pk=None):
-        pass 
+    # def destroy (self , request , pk=None):
+    #     pass 
