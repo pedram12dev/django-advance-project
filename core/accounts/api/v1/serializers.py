209abc206c55e41a5cfdaff4276741a7c3ffd2e5
Peram Najafi <pedram.najafi73@yahoo.com>
2022-09-03 19:19:07 +0430

    password validate in serializer

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from accounts.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password1 = serializers.CharField(max_length = 255 , write_only = True)
@@ -7,3 +9,13 @@ class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email' , 'password' , 'password1']
+        
+        
+    def validate(self , attrs):
+        if attrs.get('password') != attrs.get('password1'):
+            raise serializers.ValidationError({'detail' : 'password doesnt match'})
+        try:
+            validate_password(attrs.get('password'))
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({'password' : list(e.message)})
+        return super().validate(attrs)
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
+
 class RegistrationApiView(generics.GenericAPIView):
     
     serializer_class =  RegistrationSerializer
@@ -18,3 +19,4 @@ class RegistrationApiView(generics.GenericAPIView):
                 'email':serializer.validated_data['email']
            }
             return Response(data , status = status.HTTP_201_CREATED)
+        return Response(serializer.errors ,status = status.HTTP_400_BAD_REQUEST )
