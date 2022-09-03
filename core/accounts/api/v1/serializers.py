6e9ca0d15410bd6822c28a18fea9aa9367983396
Peram Najafi <pedram.najafi73@yahoo.com>
2022-09-03 18:37:16 +0430

    add password-confirm to serializer

--- core/accounts/api/v1/serializers.py
+++ core/accounts/api/v1/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from accounts.models import User
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    
+    password1 = serializers.CharField(max_length = 255 , write_only = True)
     
     class Meta:
         model = User
-        fields = ['email' , 'password']
+        fields = ['email' , 'password' , 'password1']
